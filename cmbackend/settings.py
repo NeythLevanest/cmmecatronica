aad5534c59e29eb87ba48379fda85ec7730ed8d2
Neythan Levans <neythan.astro@gmail.com>
2020-09-20 12:24:17 -0500

    actualizacion de archivo settings

--- cmbackend/settings.py
+++ cmbackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '7r3n7a69=jnic+nz9)j1@h8%lei1$vy-e$0-#9b@vvut*t$6sh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [ ]
 
 
 # Application definition
@@ -139,10 +139,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static')
+#)
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
