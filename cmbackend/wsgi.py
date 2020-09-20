b89a5371b6ac5b24a3d34e6a20d776291dca5f5c
Neythan Levans <neythan.astro@gmail.com>
2020-09-20 14:37:14 -0500

    actualizacion de librerias

--- cmbackend/wsgi.py
+++ cmbackend/wsgi.py
@@ -16,5 +16,5 @@ from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cmbackend.settings')
 
-#application = get_wsgi_application()
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
